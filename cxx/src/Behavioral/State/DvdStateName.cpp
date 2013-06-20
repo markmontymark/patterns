56c0c47fc665efdacf123d220659cc58ac312ee6
Mark Koopman <markmontymark@yahoo.com>
2013-06-20 16:21:17 -0700

    Remove bogus .cpp files

--- cxx/src/Behavioral/State/DvdStateName.cpp
+++
@@ -1,9 +0,0 @@
-0
-
-
-//DvdStateName.java - the State interface
-
-public interface DvdStateName {  
-   public void showName(DvdStateContext dvdStateContext, 
-                        String nameIn);  
-}
