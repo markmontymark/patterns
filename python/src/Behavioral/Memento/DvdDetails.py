db4104fb3c391ef871e9c4b65b4ad5fef67e2f1f
Mark Koopman <markmontymark@yahoo.com>
2013-07-29 10:42:06 -0700

    add impl for behavioral - memento

--- python/src/Behavioral/Memento/DvdDetails.py
+++ python/src/Behavioral/Memento/DvdDetails.py
@@ -9,31 +9,50 @@ from DvdMemento import DvdMemento
 class DvdDetails :
 
 	def __init__(self,titleName,stars = [],region = 1):
-		self.titleName = titleName
-		self.stars = stars
-		self.region = region
+		self.state = {}
+		self.setTitleName(titleName)
+		self.setStars(stars)
+		self.setRegion(region)
+
+	def getTitleName(self) :
+		return self.state['titleName']
+
+	def setTitleName(self,t) :
+		self.state['titleName'] = t
+
+	def getStars(self) :
+		return self.state['stars']
+
+	def setStars(self,t) :
+		self.state['stars'] = t
+
+	def getRegion(self) :
+		return self.state['region']
+
+	def setRegion(self,t) :
+		self.state['region'] = t
 
 
 	def addStar(self,star) :
-		self.stars.append(star)
+		self.state['stars'].append(star)
 
 	def starsAsString(self) :
-		return ', '.join(stars)
+		return ', '.join(self.getStars())
 
 	def formatDvdDetails(self) : 
-		return "DVD: #{0} starring: {1} encoding region: {2}".format(self.titleName,self.starsAsString(), self.region);
+		return "DVD: {0} starring: {1} encoding region: {2}".format(self.getTitleName(),self.starsAsString(), self.getRegion() );
 
 	#   //sets current state to what DvdMemento has
 	def setDvdMemento (self,memento ) :
-		for k in self:
-			self[k] = None
+		for k in self.state:
+			self.state[k] = None
 		c = memento.getState()
 		for k in c:
-			self[k] = c[k]
+			self.state[k] = c[k]
 
 	#//save current state of DvdDetails in a DvdMemento
 	def createDvdMemento (self) :
 		mem = DvdMemento()
-		mem.setState(self)
-		mem
+		mem.setState(self.state)
+		return mem
 
--- python/src/Behavioral/Memento/DvdMemento.py
+++ python/src/Behavioral/Memento/DvdMemento.py
@@ -1,8 +1,10 @@
+import copy
+
 class DvdMemento:
 	def __init__(self,cache = {}):
 		self.cache = cache;
 	def setState (self,toSave) :
-		self.cache = toSave.clone()
+		self.cache = copy.deepcopy(toSave)
 	def getState (self):
 		return self.cache
 
