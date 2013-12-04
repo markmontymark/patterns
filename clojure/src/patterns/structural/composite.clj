f9b36a726e1c9c08dbfa85e06b0184082ec54775
Mark Koopman <markmontymark@yahoo.com>
2013-12-04 15:10:35 -0800

    Adding clojure structural - composite impl and test

--- clojure/src/patterns/structural/composite.clj
+++ clojure/src/patterns/structural/composite.clj
@@ -1,44 +1,35 @@
 (ns patterns.structural.composite
    (:require [clojure.string :as str]))
 
-(defclass one-tea-bag 
-	(teabags) ())
+(defrecord teabags [name parent teabaglist])
 
-(defmethod count-teabags  ((this one-tea-bag)) 1)
-(defmethod add  ((this one-tea-bag) (tb teabags)) nil)
-(defmethod drop ((this one-tea-bag) (tb teabags)) nil)
+(defprotocol Teabagger 
+	(count-teabags [this])
+	(add-teabag    [this teabag])
+	(drop-teabag   [this teabag]))
 
-(defclass teabags
-	()
-	((teabaglist :accessor :teabaglist :initform nil)
-	 (parent :accessor :parent :initarg :parent :initform nil)
-	 (name :accessor :name :initarg :name)))
+(defrecord one-tea-bag [teabags]
+	Teabagger 
+		(count-teabags [this] 1)
+		(add-teabag    [this _] nil)
+		(drop-teabag   [this _] nil))
 
-(defgeneric count-teabags  (teabags))
-(defgeneric add  (teabagsthis teabagsthat))
-(defgeneric drop (teabagsthis teabagsthat))
-(defclass tin-of-tea-bags 
-	(teabags) ())
 
-(defmethod initialize-instance :after ((this tin-of-tea-bags) &key)
-	(setf (slot-value this 'teabaglist) ()))
-
-(defmethod count-teabags
-	((this tin-of-tea-bags))
-	(let
-		((n 0))
-		(dolist (tb (:teabaglist this))
-         (setf n (+ n (count-teabags tb))))
-		n))
+(defrecord tin-of-tea-bags [teabags]
+	Teabagger
+		(count-teabags [this]
+			(reduce  + 0 (map #(count-teabags %) @(:teabaglist (:teabags this)))))
+		(add-teabag [this tb]
+			(swap! (:parent (:teabags tb)) (constantly this))
+			(dosync
+				(commute (:teabaglist (:teabags this)) conj tb)))
+		(drop-teabag [this tb]
+			(swap! (:parent (:teabags tb)) (constantly nil))
+			(dosync
+				(commute (:teabaglist (:teabags this)) disj tb))))
    
-(defmethod add 
-	((this tin-of-tea-bags)
-	 (tb teabags))
-	(setf (:parent this) tb)
-	(push tb (:teabaglist this)))
-	
-(defmethod drop
-	((this tin-of-tea-bags)
-	 (tb teabags))
-	(setf (:teabaglist this) (remove tb (:teabaglist this))))
+(defn make-one-tea-bag [name]
+	(->one-tea-bag (->teabags name (atom nil) (ref #{}))))
 
+(defn make-tin-of-tea-bags [name]
+	(->tin-of-tea-bags (->teabags name (atom nil) (ref #{}))))
--- clojure/test/patterns/structural/composite_test.clj
+++ clojure/test/patterns/structural/composite_test.clj
@@ -7,27 +7,32 @@
 
 (deftest structural-composite-test
    (let
-   [a-tin-of-tea-bags (make-tin-of-tea-bags :name "tin of tea bags")
-    small-tin-of-tea-bags (make-tin-of-tea-bags :name "small tin of tea bags")
-	 teabag-1 (make-one-tea-bag :name "tea bag 1")
-	 teabag-2 (make-one-tea-bag :name "tea bag 2")
-	 teabag-3 (make-one-tea-bag :name "tea bag 3")]
+   [a-tin-of-tea-bags (make-tin-of-tea-bags "tin of tea bags")
+    small-tin-of-tea-bags (make-tin-of-tea-bags "small tin of tea bags")
+	 teabag-1 (make-one-tea-bag "tea bag 1")
+	 teabag-2 (make-one-tea-bag "tea bag 2")
+	 teabag-3 (make-one-tea-bag "tea bag 3")]
 
-   (add a-tin-of-tea-bags teabag-1)
-   (add a-tin-of-tea-bags teabag-2)
-	(testing "" (is (= 2 (count-teabags a-tin-of-tea-bags))))
+   (add-teabag a-tin-of-tea-bags teabag-1)
+   (add-teabag a-tin-of-tea-bags teabag-2)
 
-   (add small-tin-of-tea-bags teabag-3)
+	(testing "After adding two tea bags to a tin" 
+		(is (= 2 (count-teabags a-tin-of-tea-bags))))
 
-	(testing "" (is (= 1 (count-teabags small-tin-of-tea-bags))))
+   (add-teabag small-tin-of-tea-bags teabag-3)
 
-   (add a-tin-of-tea-bags small-tin-of-tea-bags)
+	(testing "After adding a tea bag to different tin" 
+		(is (= 1 (count-teabags small-tin-of-tea-bags))))
 
-	(testing "" (is (= 3 (count-teabags a-tin-of-tea-bags))))
+   (add-teabag a-tin-of-tea-bags small-tin-of-tea-bags)
 
-	(drop a-tin-of-tea-bags teabag-2)
+	(testing "After adding a tin of teabags into another tin of tea bags" 
+		(is (= 3 (count-teabags a-tin-of-tea-bags))))
 
-	(testing "" (is (= 2 (count-teabags a-tin-of-tea-bags))))
+	(drop-teabag a-tin-of-tea-bags teabag-2)
+
+	(testing "Drop a single tea bag from a tin with a tin and a tea bag" 
+		(is (= 2 (count-teabags a-tin-of-tea-bags))))
 ))
 
 
